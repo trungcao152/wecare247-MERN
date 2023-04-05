a64cf361265a44acea0fc4624240bd7e20a5cbba
trungcao152 <trungcb152@gmail.com>
2023-04-05 12:05:40 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,12 @@ app.use((req, res, next) => {
   next();
 });
 if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("frontend/build"));
+  // Set static folder up in production
+  const frontendBuildPath = path.join(__dirname, "..", "frontend", "build");
+  app.use(express.static(frontendBuildPath));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(frontendBuildPath, "index.html"))
   );
 }
 
