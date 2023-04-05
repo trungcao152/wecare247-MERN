16e17a3d349f68c75f236354db1b0940f6393815
trungcao152 <trungcb152@gmail.com>
2023-04-05 11:56:05 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const caregiverRoutes = require("./routes/caregivers");
+const path = require("path");
 
 // express app
 const app = express();
