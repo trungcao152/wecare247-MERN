795bc1ef3feb2467b94f84e75925ce5395d3145c
trungcao152 <trungcb152@gmail.com>
2023-04-14 16:47:52 +0700

    Update 1 for refracting

--- frontend/src/components/CaregiversTable.js
+++ frontend/src/components/CaregiversTable.js
@@ -1,8 +1,47 @@
 import React from "react";
 import TableHead from "./TableHead";
 import TableBody from "./TableBody";
+import EditCaregiverForm from "./EditCaregiverForm";
+import { useCaregiversContext } from "./hooks/useCaregiversContext";
+
+const CaregiverRow = ({
+  caregiver,
+  handleDelete,
+  properties,
+  formatCellContent,
+}) => {
+  const [isEditing, setIsEditing] = React.useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <tr key={caregiver._id}>
+      {isEditing ? (
+        <td colSpan={properties.length + 1}>
+          <EditCaregiverForm
+            caregiver={caregiver}
+            setIsEditing={setIsEditing}
+          />
+        </td>
+      ) : (
+        <>
+          {properties.map((property) => (
+            <td key={property}>{formatCellContent(caregiver, property)}</td>
+          ))}
+          <td>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={() => handleDelete(caregiver._id)}>Delete</button>
+          </td>
+        </>
+      )}
+    </tr>
+  );
+};
 
 const CaregiversTable = ({ handleDelete, tableTitle }) => {
+  const { caregivers } = useCaregiversContext();
   const columns = [
     "ID",
     "Name",
@@ -33,10 +72,30 @@ const CaregiversTable = ({ handleDelete, tableTitle }) => {
     "age",
   ];
 
+  const formatCellContent = (caregiver, property) => {
+    if (property === "national_id_issue_date") {
+      return new Intl.DateTimeFormat("en-GB").format(
+        new Date(caregiver[property])
+      );
+    }
+    return caregiver[property];
+  };
+
   return (
     <table>
       <TableHead columns={columns} tableTitle={tableTitle} />
-      <TableBody handleDelete={handleDelete} properties={properties} />
+      <TableBody>
+        {caregivers &&
+          caregivers.map((caregiver) => (
+            <CaregiverRow
+              key={caregiver._id}
+              caregiver={caregiver}
+              handleDelete={handleDelete}
+              properties={properties}
+              formatCellContent={formatCellContent}
+            />
+          ))}
+      </TableBody>
     </table>
   );
 };
--- frontend/src/components/TableBody.js
+++ frontend/src/components/TableBody.js
@@ -1,63 +1,7 @@
 import React from "react";
-import { useCaregiversContext } from "./hooks/useCaregiversContext";
-import EditCaregiverForm from "./EditCaregiverForm";
 
-const TableRow = ({ caregiver, handleDelete, properties, formattedDate }) => {
-  const [isEditing, setIsEditing] = React.useState(false);
-
-  const handleEdit = () => {
-    setIsEditing(!isEditing);
-  };
-
-  return (
-    <tr key={caregiver._id}>
-      {isEditing ? (
-        <td colSpan={properties.length + 1}>
-          <EditCaregiverForm
-            caregiver={caregiver}
-            setIsEditing={setIsEditing}
-          />
-        </td>
-      ) : (
-        <>
-          {properties.map((property) => (
-            <td key={property}>
-              {property === "national_id_issue_date"
-                ? formattedDate(caregiver[property])
-                : caregiver[property]}
-            </td>
-          ))}
-          <td>
-            <button onClick={handleEdit}>Edit</button>
-            <button onClick={() => handleDelete(caregiver._id)}>Delete</button>
-          </td>
-        </>
-      )}
-    </tr>
-  );
-};
-
-const TableBody = ({ handleDelete, properties }) => {
-  const { caregivers } = useCaregiversContext();
-
-  const formattedDate = (date) => {
-    return new Intl.DateTimeFormat("en-GB").format(new Date(date));
-  };
-
-  return (
-    <tbody>
-      {caregivers &&
-        caregivers.map((caregiver) => (
-          <TableRow
-            key={caregiver._id}
-            caregiver={caregiver}
-            handleDelete={handleDelete}
-            properties={properties}
-            formattedDate={formattedDate}
-          />
-        ))}
-    </tbody>
-  );
+const TableBody = ({ children }) => {
+  return <tbody>{children}</tbody>;
 };
 
 export default TableBody;
