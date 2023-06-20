1359a30a0f07433e8684a53913798de74cd0b353
trungcao152 <trungcb152@gmail.com>
2023-06-20 19:40:24 +0700

    Update 9

---
+++ frontend/src/context/ProductsContext.js
@@ -0,0 +1,41 @@
+import { createContext, useReducer } from "react";
+
+export const ProductsContext = createContext();
+
+export const productsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_PRODUCTS":
+      return {
+        products: action.payload,
+      };
+    case "CREATE_PRODUCT":
+      return {
+        products: [action.payload, ...state.products],
+      };
+    case "DELETE_PRODUCT":
+      return {
+        products: state.products.filter((c) => c._id !== action.payload._id),
+      };
+    case "UPDATE_PRODUCT":
+      return {
+        products: state.products.map((c) =>
+          c._id === action.payload._id ? action.payload : c
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const ProductsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(productsReducer, {
+    products: null,
+  });
+
+  return (
+    <ProductsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
